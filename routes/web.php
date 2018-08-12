1924b205a2d2f407e2dc50229161daaf2686863c
Janek Pilzecker <janek.pilzecker@gmx.de>
2018-08-12 11:45:21 +0200

    bugfix routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 // General Pages
 Route::get('/', 'IndexController@showHome');
-Route::get('/imprint', 'IndexController@showFaq');
+Route::get('/impressum', 'IndexController@showFaq');
 Route::view('/news', 'news'); 
 Route::view('error', 'error');
 
@@ -25,35 +25,35 @@ Route::get('bands/{id}', 'IndexController@showBandDetailPage');
 
 // dashboard
 Route::prefix('/dash')->group(function () {
-	Route::get('/home', 'HomeController@index')->middleware('auth');
-	Route::get('/profile', 'HomeController@showProfile')->middleware('auth');
-	Route::get('/band', 'HomeController@showUserBand')->middleware('auth');
-	Route::post('/band/register', 'HomeController@registerBand')->middleware('auth');
-	Route::get('/media', 'HomeController@showMedia')->middleware('auth');
-	Route::get('/settings', 'HomeController@showSettings')->middleware('auth');
+	Route::get('/home', 'HomeController@index');
+	Route::get('/profile', 'HomeController@showProfile');
+	Route::get('/band', 'HomeController@showUserBand');
+	Route::post('/band/register', 'HomeController@registerBand');
+	Route::get('/media', 'HomeController@showMedia');
+	Route::get('/settings', 'HomeController@showSettings');
 });
 
 // media
-Route::post('userimageupload','ImageUploadController@userImageUpload')->middleware('auth');
-Route::post('profileimageupload','ImageUploadController@profileImageUpload')->middleware('auth');
-Route::post('/bandimageupload','ImageUploadController@bandImageUpload')->middleware('auth');
-Route::post('gigimageupload','ImageUploadController@gigImageUpload')->middleware('auth');
+Route::post('userimageupload','ImageUploadController@userImageUpload');
+Route::post('profileimageupload','ImageUploadController@profileImageUpload');
+Route::post('/bandimageupload','ImageUploadController@bandImageUpload');
+Route::post('gigimageupload','ImageUploadController@gigImageUpload');
 
 // gigs
 Route::get('/dash/gigs', 'HomeController@showGigs');
 Route::post('/registergig', 'GigController@registerGig');
 
 // Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login')->middleware('auth');
-Route::post('login', 'Auth\LoginController@login')->middleware('auth');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout')->middleware('auth');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@store')->middleware('auth');
+Route::post('register', 'Auth\RegisterController@store');
 
 // Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request')->middleware('auth');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email')->middleware('auth');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset')->middleware('auth');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->middleware('auth');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
